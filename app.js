555e55a877a31fccc21b77ad7daf72e60e54bf1c
Antonio <antonio@engsolutions.com.br>
2017-07-27 11:29:09 -0300

    Tratativa de erros que estão dentro de Promises

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
 const apiRequest = require('./apiRequest');
+
 module.exports = (options) => {
+    handlePromises(options);
+    handleGlobal(options);
+    console.log(`[Shazam-Middleware] Activated`);
     return {
         log: (req, res, next) => {
-            console.log(`[${req.method}] ${req.protocol + '://' + req.get('host') + req.originalUrl} | Data: ${new Date().toLocaleString()} | IP: ${getip(req)}`);
+            console.log(`[Shazam-Middleware] ${req.method} ${req.protocol + '://' + req.get('host') + req.originalUrl} | Data: ${new Date().toLocaleString()} | IP: ${getip(req)}`);
             next();
         },
         exception: (err, req, res, next) => {
             let date = new Date();
-            console.error(`[${req.method}] ${req.protocol + '://' + req.get('host') + req.originalUrl} | Erro: ${err.message} | Data: ${date.toLocaleString()} | IP: ${getip(req)}`);
+            console.error(`[Shazam-Middleware] ${req.method} ${req.protocol + '://' + req.get('host') + req.originalUrl} | Erro: ${err.message} | Data: ${date.toLocaleString()} | IP: ${getip(req)}`);
 
-            let paramters = {
-                url: options.slack.urlHook,
-                method: 'POST',
-                body: {
-                    text: "*" + options.api.name + " - " + options.api.version + "*",
-                    username: (options.slack.botusername || "Shazam"),
-                    channel: options.slack.channel,
-                    icon_url: (options.slack.iconUrl || "http://dclegends.wiki/images/d/d9/Shazam_Billy_Batson_Portrait.png"),
-                    attachments: [
-                        {
-                            color: "#ff0000",
-                            title: err.message,
-                            title_link: `${req.protocol}://${req.get('host')}${req.originalUrl}`,
-                            footer: `[${req.method}] - ${req.originalUrl}`,
-                            ts: Math.round(date.getTime() / 1000),
-                            pretext: "```" + err.stack + "```",
-                            mrkdwn_in: ["text", "pretext", "title"]
-                        }
-                    ]
-                },
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8'
-                },
-                json: true
-            };
 
-            apiRequest.requestApi(paramters)
-                .then(q => {
-                    console.log(`Notificado no #channel ${options.slack.channel}`);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+            if (options.slack) {
+                let attachments = [
+                    {
+                        color: "#ff0000",
+                        title: err.message,
+                        title_link: `${req.protocol}://${req.get('host')}${req.originalUrl}`,
+                        footer: `[${req.method}] - ${req.originalUrl}`,
+                        ts: Math.round(date.getTime() / 1000),
+                        pretext: "```" + err.stack + "```",
+                        mrkdwn_in: ["text", "pretext", "title"],
+                        fields: [
+                            {
+                                "title": "Error Type",
+                                "value": "Global",
+                                "short": true
+                            },
+                            {
+                                "title": "Environment",
+                                "value": process.env.NODE_ENV,
+                                "short": true
+                            }
+                        ],
+                    }
+                ];
+                sendSlack(options, attachments);
+            }
+
 
             if (res.headersSent || res.finished)
                 return;
@@ -49,7 +48,8 @@ module.exports = (options) => {
             res.status(500).send({
                 message: {
                     developerMessage: err.message,
-                    userMessage: `Ocorreu uma falha inesperada pela aplicação, mas não se preocupe, ela já foi reportada automaticamente para os desenvolvedores.`,
+                    userMessage: `An unexpected crash occurred by the application, but do not worry, it has already been automatically reported to developers.`,
+                    userMessageBr: `Ocorreu uma falha inesperada pela aplicação, mas não se preocupe, ela já foi reportada automaticamente para os desenvolvedores.`,
                 },
                 isSuccess: false,
                 details: {
@@ -64,9 +64,103 @@ module.exports = (options) => {
     }
 };
 
+
+function sendSlack(options, attachments) {
+    let paramters = {
+        url: options.slack.urlHook,
+        method: 'POST',
+        body: {
+            text: "*" + options.api.name + " - " + options.api.version + "*",
+            username: (options.slack.botusername || "Shazam"),
+            channel: options.slack.channel,
+            icon_url: (options.slack.iconUrl || "http://dclegends.wiki/images/d/d9/Shazam_Billy_Batson_Portrait.png"),
+            attachments: attachments
+        },
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        json: true
+    };
+
+    apiRequest.requestApi(paramters)
+        .then(q => {
+            console.log(`[Shazam-Middleware] Notified on #channel ${options.slack.channel}`);
+        })
+        .catch(err => {
+            console.log(`[Shazam-Middleware] ${err}`);
+        });
+
+}
 function getip(req) {
     return req.ip ||
         req._remoteAddress ||
         (req.connection && req.connection.remoteAddress) ||
         undefined
+}
+
+let handleGlobal = (options) => {
+    process.on('uncaughtException', (err) => {
+        let date = new Date();
+
+        console.error(`[Shazam-Middleware] Global Error | ${err.message}\nStack:\n${err.stack}`);
+
+        if (options.slack) {
+            let attachments = [
+                {
+                    color: "#ff0000",
+                    title: err.message,
+                    ts: Math.round(date.getTime() / 1000),
+                    pretext: "```" + err.stack + "```",
+                    mrkdwn_in: ["text", "pretext", "title"],
+                    fields: [
+                        {
+                            "title": "Error Type",
+                            "value": "Global Error",
+                            "short": true
+                        },
+                        {
+                            "title": "Environment",
+                            "value": (options.env.NODE_ENV || process.env.NODE_ENV),
+                            "short": true
+                        }
+                    ],
+
+                }
+            ];
+            sendSlack(options, attachments);
+        }
+    });
+}
+let handlePromises = (options) => {
+    process.on('unhandledRejection', (err, p) => {
+        let date = new Date();
+
+        console.error(`[Shazam-Middleware] Promise Error | ${err.message}\nStack:\n${err.stack}`);
+
+        if (options.slack) {
+            let attachments = [
+                {
+                    color: "#ff0000",
+                    title: err.message,
+                    ts: Math.round(date.getTime() / 1000),
+                    pretext: "```" + err.stack + "```",
+                    mrkdwn_in: ["text", "pretext", "title"],
+                    fields: [
+                        {
+                            "title": "Error Type",
+                            "value": "Global Error",
+                            "short": true
+                        },
+                        {
+                            "title": "Environment",
+                            "value": (options.env.NODE_ENV || process.env.NODE_ENV),
+                            "short": true
+                        }
+                    ],
+
+                }
+            ];
+            sendSlack(options, attachments);
+        }
+    });
 }
