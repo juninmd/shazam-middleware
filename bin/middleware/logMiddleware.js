4d19948231bd22fe548975fbcb54471106667d43
juninmd <jr_acn@yahoo.com.br>
2017-09-18 20:23:24 -0300

    Correção Log agent

--- bin/middleware/logMiddleware.js
+++ bin/middleware/logMiddleware.js
@@ -1,7 +1,6 @@
 const onFinished = require('on-finished')
 const ipUtil = require('../util/ipUtil');
 const browser = require('browser-detect');
-const slackAttachment = require('../util/slackAttachment');
 
 let t0 = undefined;
 
@@ -20,29 +19,35 @@ const dateDiff = (d) => {
         d -= r[key] * s[key];
     });
 
-    return r;
+    return `${r.hour > 0 ? r.hour + " hour(s) " : ""}${r.minute > 0 ? r.minute + " minute(s) " : ""}${r.second > 0 ? r.second + " second(s) " : ""}${r.ms > 0 ? r.ms + " ms" : ""}`;
 };
 
-const dateDiffString = (r) => {
-    return `${r.hour > 0 ? r.hour + " hour(s) " : ""}${r.minute > 0 ? r.minute + " minute(s) " : ""}${r.second > 0 ? r.second + " second(s) " : ""}${r.ms > 0 ? r.ms + " ms" : ""}`;
-}
+const checkBrowser = (agent) => {
 
+    if (agent == undefined) {
+        return {
+            mobile: false,
+            name: '?',
+            version: ''
+        }
+    }
+    
+    if (agent.indexOf('PostmanRuntime') == 0) {
+        let postman = agent.split('/')
+        return {
+            mobile: false,
+            name: postman[0],
+            version: postman[1]
+        }
+    }
+    return browser(agent);
+}
 const logRequest = (err, result) => {
     let t1 = process.hrtime(t0);
-    let date = dateDiff((t1[0], t1[1] / 1000000).toString());
-    let response = dateDiffString(date);
-    let browserN = browser(result.req.headers['user-agent']);
-    let message = `[ShazaM] ${result.req.method} | ${result.statusCode} | ${result.req.protocol + '://' + result.req.get('host') + result.req.originalUrl} | IP: ${ipUtil.getip(result.req)} | Response: ${response} | ${browserN.name} ${browserN.version}`;
+    let data = dateDiff((t1[0], t1[1] / 1000000).toString());
+    let browserN = checkBrowser(result.req.headers['user-agent']);
+    let message = `[ShazaM] ${result.req.method} | ${result.statusCode} | ${result.req.protocol + '://' + result.req.get('host') + result.req.originalUrl} | IP: ${ipUtil.getip(result.req)} | Response: ${data} | ${browserN.name} ${browserN.version}`;
     console.log(`${message}`);
-
-    // Request long
-    if (date.second > 2) {
-        options.customize = {
-            errortype: "lazy Request",
-            color: 'yellow'
-        }
-        slackAttachment(err, result.req, date, options);
-    }
 }
 
 module.exports = (options) => {
